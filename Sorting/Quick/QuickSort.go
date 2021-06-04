abee8b54cb8095380b99371a5db13b3541caddfb
yatender-oktalk <yatender.nitk@gmail.com>
2021-06-04 17:55:34 +0530

    Removing unwanted code

--- Sorting/Quick/QuickSort.go
+++ Sorting/Quick/QuickSort.go
@@ -7,79 +7,7 @@ import (
 var arr = []int{8, 5, 2, 9, 5, 6, 3}
 
 func main() {
-	fmt.Println(QuickSort(arr))
-}
-
-func QuickSort(a []int) []int {
-	quickSort(a, 0, len(a)-1)
-	return a
-}
-
-func Partition(arr []int, low, high int) int {
-	if low >= high {
-		return arr
-	}
-	fmt.Println(arr)
-	pivot := arr[low]
-	i := low
-	j := high
-
-	for i < j {
-		for arr[i] <= pivot && i < high {
-			i++
-		}
-
-		for arr[j] > pivot && j > low {
-			j--
-		}
-
-		if i < j {
-			arr[i], arr[j] = arr[j], arr[i]
-		}
-	}
-	arr[low], arr[j] = arr[j], pivot
-	return j
-}
-
-func quickSort(arr []int, low, high int) {
-	if low >= high {
-		return
-	}
-	var pivot = PartitionO(arr, low, high)
-	quickSort(arr, low, pivot)
-	quickSort(arr, pivot+1, high)
-
-}
-
-func qsort(a []int) []int {
-	if len(a) < 2 {
-		return a
-	}
-
-	left, right := 0, len(a)-1
-
-	// Pick a pivot
-	pivotIndex := 5 % len(a)
-
-	// Move the pivot to the right
-	a[pivotIndex], a[right] = a[right], a[pivotIndex]
-
-	// Pile elements smaller than the pivot on the left
-	for i := range a {
-		if a[i] < a[right] {
-			a[i], a[left] = a[left], a[i]
-			left++
-		}
-	}
-
-	// Place the pivot after the last smaller element
-	a[left], a[right] = a[right], a[left]
-
-	// Go down the rabbit hole
-	qsort(a[:left])
-	qsort(a[left+1:])
-
-	return a
+	fmt.Println(MyQuickSort(arr))
 }
 
 func MyQuickSort(a []int) []int {
@@ -111,27 +39,4 @@ func MyQuickSort(a []int) []int {
 	MyQuickSort(a[leftIndex+1:])
 
 	return a
-}
-
-func PartitionO(arr []int, low, high int) int {
-	var pivot = arr[low]
-	var i = low
-	var j = high
-
-	for i < j {
-		for arr[i] <= pivot && i < high {
-			i++
-		}
-		for arr[j] > pivot && j > low {
-			j--
-		}
-
-		if i < j {
-			arr[j], arr[i] = arr[i], arr[j]
-		}
-	}
-
-	arr[low], arr[j] = arr[j], pivot
-
-	return j
 }
