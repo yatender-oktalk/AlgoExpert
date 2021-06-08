162c2a911263639b2c87d6c219166c84083e62f5
yatender-oktalk <yatender.nitk@gmail.com>
2021-06-08 17:59:40 +0530

    Added the quick Union

--- DataStructures/QuickFind/QuickFind.go
+++ DataStructures/QuickFind/QuickFind.go
@@ -4,13 +4,16 @@ import "fmt"
 
 func main() {
 	arr := Initialize(10)
-	fmt.Println(IsConnected(3, 4, arr))
+	fmt.Println(IsConnectedQuick(3, 4, arr))
 	fmt.Println(arr)
-	arr = Union(3, 4, arr)
+	arr = UnionQuick(3, 4, arr)
 	fmt.Println(arr)
-	fmt.Println(IsConnected(3, 4, arr))
-	arr = Union(3, 9, arr)
-	arr = Union(3, 8, arr)
+	fmt.Println(IsConnectedQuick(3, 4, arr))
+	arr = UnionQuick(3, 9, arr)
+	fmt.Println(arr)
+	fmt.Println(IsConnectedQuick(3, 9, arr))
+	arr = UnionQuick(3, 8, arr)
+	fmt.Println(IsConnectedQuick(3, 8, arr))
 	fmt.Println(arr)
 
 }
@@ -39,4 +42,24 @@ func Union(n, m int, arr []int) []int {
 		}
 	}
 	return arr
+}
+
+// Quick Union Improves
+func UnionQuick(n, m int, arr []int) []int {
+	i := root(n, arr)
+	j := root(m, arr)
+	arr[i] = j
+	return arr
+}
+
+func IsConnectedQuick(n, m int, arr []int) bool {
+	return root(n, arr) == root(m, arr)
+}
+
+// This private function will give back the root node
+func root(n int, arr []int) int {
+	for arr[n] != n {
+		n = arr[n]
+	}
+	return n
 }
