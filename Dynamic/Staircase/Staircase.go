b212667aef87dfabbb1019d8d40788be4b5f0a5b
yatender-oktalk <yatender.nitk@gmail.com>
2021-06-08 17:05:30 +0530

    Added the dynamic staircase

--- Dynamic/Staircase/Staircase.go
+++ Dynamic/Staircase/Staircase.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
+var stairMemoized = make(map[string]int)
+
 func main() {
 	var n = 10
-	var maxSteps = 3
+	var maxSteps = 2
 	fmt.Println(Staircase(n, maxSteps))
 }
 
 func Staircase(n int, maxSteps int) int {
+	key := getKey(n, maxSteps)
+	if keyAvailableInMap(key) {
+		return stairMemoized[key]
+	}
 	// user can take 1 step or 2 step not more than that
 	if n <= 1 {
 		return 1
@@ -19,7 +25,12 @@ func Staircase(n int, maxSteps int) int {
 		x += Staircase(n-i, maxSteps)
 	}
 
+	stairMemoized[key] = x
 	return x
+}
+
+func getKey(n, m int) string {
+	return fmt.Sprintf("%d-%d", n, m)
 }
 
 func min(a, b int) int {
@@ -28,4 +39,9 @@ func min(a, b int) int {
 	} else {
 		return a
 	}
+}
+
+func keyAvailableInMap(key string) bool {
+	_, isKeyAvailable := stairMemoized[key]
+	return isKeyAvailable
 }
