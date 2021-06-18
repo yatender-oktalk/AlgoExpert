8bcb65c8f1421fde023b103a82aa3812d23b5f87
yatender-oktalk <yatender.nitk@gmail.com>
2021-06-18 19:39:12 +0530

    Array of products solved

---
+++ ArrayOfProducts/ArrayOfProducts.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+var arr []int = []int{5, 1, 4, 2}
+
+func main() {
+	fmt.Println(ArrayProduct(arr))
+}
+
+func ArrayProduct(arr []int) []int {
+	// Final Product array
+
+	productArr := make([]int, len(arr))
+	leftArr := make([]int, len(arr))
+	rightArr := make([]int, len(arr))
+
+	// Initialize everything to 1
+
+	for i := range arr {
+		productArr[i] = 1
+		leftArr[i] = 1
+		rightArr[i] = 1
+	}
+
+	leftRunningProduct := 1
+	// let's store the left product in left arr
+	for i := range arr {
+		leftArr[i] = leftRunningProduct
+		leftRunningProduct *= arr[i]
+	}
+
+	rightRunningProduct := 1
+	// similarly define the right array from reverse pointer
+	for i := len(arr) - 1; i >= 0; i-- {
+		rightArr[i] = rightRunningProduct
+		rightRunningProduct *= arr[i]
+	}
+
+	for i := range arr {
+		productArr[i] = rightArr[i] * leftArr[i]
+	}
+	return productArr
+}
